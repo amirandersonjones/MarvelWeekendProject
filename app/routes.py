47950bf1b49e0f69d048cc98acf8fe9649bd39c1
amirandersonjones <amirandersonjones@gmail.com>
2022-03-19 15:06:26 -0500

    moreflaskjinja

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,18 @@
 from app import app
 from flask import render_template
 
+#this is for my variable behavior in aboutmarvel html
+from random import choice
+
 @app.route('/')
 @app.route('/marvelhomepage') #decorator says this is a route of the flask app 'app' with the url endpoint '/marvelhomepage'
 def home():
-    return render_template('index.html')
+    headline= 'HELLO MARVEL FANS, WELCOME TO THE MARVEL HOMEPAGE'
+    favorites = ['GREEN GOBLIN','MAGNETO', 'RED SKULL', 'LOKI']
+    return render_template('index.html', headline=headline, favorites=favorites)
 
 @app.route('/aboutmarvel')
 def aboutmarvel():
-    return render_template('aboutmarvel.html')
+    headline= 'THIS IS ABOUT THE HISTORY OF MARVEL'
+    x= choice(['WELCOME FANS', 'THANKS FOR VISITING', 'WE APPRECIATE YOUR LOYAL SUPPORT'])
+    return render_template('aboutmarvel.html', headline=headline, greetings=x)
